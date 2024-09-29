53697f0075d28924be57ae326d2c1bc9b755c33f
Steven Casper <sebastiancasper3@gmail.com>
2024-09-29 17:53:46 -0400

    Assert on surface format

--- VEE/Source/Renderer.cpp
+++ VEE/Source/Renderer.cpp
@@ -255,6 +255,7 @@ Renderer::Renderer(const Platform::Window& window) {
             break;
         }
     }
+    assert(format == VK_FORMAT_B8G8R8A8_SRGB);
 
     VkSurfaceCapabilitiesKHR surface_capabilities = {};
     VK_CHECK(vkGetPhysicalDeviceSurfaceCapabilitiesKHR(gpu, surface, &surface_capabilities));
