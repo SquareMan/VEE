1c9227e9e050476cfce9318004f987e53a3f57dc
Steven Casper <sebastiancasper3@gmail.com>
2024-10-27 16:41:15 -0400

    vkb swapchain

--- Source/VeeRuntime/Private/Renderer/Swapchain.cpp
+++ Source/VeeRuntime/Private/Renderer/Swapchain.cpp
@@ -4,6 +4,8 @@
 
 #include "Renderer/Swapchain.hpp"
 
+#include <VkBootstrap.h>
+
 namespace vee {
 Swapchain::Swapchain(
     vk::PhysicalDevice gpu,
@@ -17,41 +19,28 @@ Swapchain::Swapchain(
     , width(width)
     , height(height)
     , device(device) {
-    // swapchain
-    vk::SurfaceCapabilitiesKHR surface_capabilities = gpu.getSurfaceCapabilitiesKHR(surface).value;
+    vkb::Swapchain swapchain =
+        vkb::SwapchainBuilder(gpu, device, surface)
+            .set_desired_present_mode(VK_PRESENT_MODE_FIFO_RELAXED_KHR)
+            .set_desired_min_image_count(vkb::SwapchainBuilder::TRIPLE_BUFFERING)
+            .add_image_usage_flags(VK_IMAGE_USAGE_TRANSFER_DST_BIT)
+            .build()
+            .value();
 
-    uint32_t img_count = surface_capabilities.minImageCount + 1;
-    img_count = img_count > surface_capabilities.maxImageCount ? img_count - 1 : img_count;
-
-    const vk::SwapchainCreateInfoKHR swapchain_info(
-        {},
-        surface,
-        img_count,
-        format,
-        {},
-        surface_capabilities.currentExtent,
-        1,
-        vk::ImageUsageFlagBits::eColorAttachment | vk::ImageUsageFlagBits::eTransferDst,
-        {},
-        {},
-        surface_capabilities.currentTransform,
-        vk::CompositeAlphaFlagBitsKHR::eOpaque,
-        // TODO: Allow eMailbox in the future, maybe make it the default
-        vk::PresentModeKHR::eFifoRelaxed
+    handle = swapchain.swapchain;
+    std::ranges::transform(
+        swapchain.get_images().value(),
+        std::back_inserter(images),
+        [](const VkImage& image) { return image; }
     );
-    handle = device.createSwapchainKHR(swapchain_info).value;
-
-
-    images = device.getSwapchainImagesKHR(handle).value;
-
-    image_views.reserve(images.size());
-    vk::ImageViewCreateInfo image_view_info(
-        {}, {}, vk::ImageViewType::e2D, format, {}, {vk::ImageAspectFlagBits::eColor, {}, 1, {}, 1}
+    std::ranges::transform(
+        swapchain.get_image_views().value(),
+        std::back_inserter(image_views),
+        [](const VkImageView& view) { return view; }
     );
-    for (vk::Image swapchain_image : images) {
-        image_view_info.image = swapchain_image,
-        image_views.push_back(device.createImageView(image_view_info).value);
-    }
+    format = static_cast<vk::Format>(swapchain.image_format);
+    width = swapchain.extent.width;
+    height = swapchain.extent.height;
 }
 
 Swapchain::~Swapchain() {
@@ -60,4 +49,4 @@ Swapchain::~Swapchain() {
     }
     device.destroySwapchainKHR(handle);
 }
-} // namespace Vee
+} // namespace vee
