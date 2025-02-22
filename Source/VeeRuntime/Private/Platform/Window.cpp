2a2e086f21401db8e2b47672c73d279967be5b63
Steven Casper <sebastiancasper3@gmail.com>
2025-02-22 14:57:15 -0500

    Basic immediate-mode input api

--- Source/VeeRuntime/Private/Platform/Window.cpp
+++ Source/VeeRuntime/Private/Platform/Window.cpp
@@ -5,10 +5,13 @@
 #include "Platform/Window.hpp"
 
 #include "Assert.hpp"
-#include "GLFW/glfw3.h"
+#include <functional>
+#include <GLFW/glfw3.h>
 
 #define GLFW_EXPOSE_NATIVE_WIN32
 #include "GLFW/glfw3native.h"
+
+#include <magic_enum/magic_enum.hpp>
 
 namespace vee::platform {
 Window::Window(const char* title, int32_t width, int32_t height) {
@@ -23,6 +26,9 @@ Window::Window(const char* title, int32_t width, int32_t height) {
         VASSERT(false, "failed to create GLFW window");
         return;
     }
+
+    glfwSetWindowUserPointer(glfw_window, this);
+    glfwSetKeyCallback(glfw_window, &Window::key_callback_dispatcher);
 }
 
 Window::~Window() {
@@ -46,5 +52,166 @@ WindowHandle Window::get_handle() const {
     VASSERT(glfw_window != nullptr, "GLFW window was not initialized");
     // TODO: not platform independent
     return glfwGetWin32Window(glfw_window);
+}
+
+bool Window::is_key_down(Key key) const {
+    return down_keys.contains(key);
+}
+
+void Window::key_callback_dispatcher(GLFWwindow* glfw_window, int key, int scancode, int action, int mods) {
+    void* window = glfwGetWindowUserPointer(glfw_window);
+    VASSERT(window != nullptr, "GLFW Window user data should contain a pointer to the vee::platform::Window class that owns it.");
+
+    Window* window_ptr = static_cast<Window*>(window);
+    window_ptr->key_callback(key, scancode, action, mods);
+}
+
+static bool key_valid(Key key) {
+    switch (key) {
+    case Key::Unknown:
+    case Key::Space:
+    case Key::Apostrophe:
+    case Key::Comma:
+    case Key::Minus:
+    case Key::Period:
+    case Key::Slash:
+    case Key::Num0:
+    case Key::Num1:
+    case Key::Num2:
+    case Key::Num3:
+    case Key::Num4:
+    case Key::Num5:
+    case Key::Num6:
+    case Key::Num7:
+    case Key::Num8:
+    case Key::Num9:
+    case Key::Semicolon:
+    case Key::Equal:
+    case Key::A:
+    case Key::B:
+    case Key::C:
+    case Key::D:
+    case Key::E:
+    case Key::F:
+    case Key::G:
+    case Key::H:
+    case Key::I:
+    case Key::J:
+    case Key::K:
+    case Key::L:
+    case Key::M:
+    case Key::N:
+    case Key::O:
+    case Key::P:
+    case Key::Q:
+    case Key::R:
+    case Key::S:
+    case Key::T:
+    case Key::U:
+    case Key::V:
+    case Key::W:
+    case Key::X:
+    case Key::Y:
+    case Key::Z:
+    case Key::Left_Bracket:
+    case Key::Backslash:
+    case Key::Right_Bracket:
+    case Key::Grave_Accent:
+    case Key::World_1:
+    case Key::World_2:
+    case Key::Escape:
+    case Key::Enter:
+    case Key::Tab:
+    case Key::Backspace:
+    case Key::Insert:
+    case Key::Delete:
+    case Key::Right:
+    case Key::Left:
+    case Key::Down:
+    case Key::Up:
+    case Key::Page_Up:
+    case Key::Page_Down:
+    case Key::Home:
+    case Key::End:
+    case Key::Caps_Lock:
+    case Key::Scroll_Lock:
+    case Key::Num_Lock:
+    case Key::Print_Screen:
+    case Key::Pause:
+    case Key::F1:
+    case Key::F2:
+    case Key::F3:
+    case Key::F4:
+    case Key::F5:
+    case Key::F6:
+    case Key::F7:
+    case Key::F8:
+    case Key::F9:
+    case Key::F10:
+    case Key::F11:
+    case Key::F12:
+    case Key::F13:
+    case Key::F14:
+    case Key::F15:
+    case Key::F16:
+    case Key::F17:
+    case Key::F18:
+    case Key::F19:
+    case Key::F20:
+    case Key::F21:
+    case Key::F22:
+    case Key::F23:
+    case Key::F24:
+    case Key::F25:
+    case Key::Kp_0:
+    case Key::Kp_1:
+    case Key::Kp_2:
+    case Key::Kp_3:
+    case Key::Kp_4:
+    case Key::Kp_5:
+    case Key::Kp_6:
+    case Key::Kp_7:
+    case Key::Kp_8:
+    case Key::Kp_9:
+    case Key::Kp_Decimal:
+    case Key::Kp_Divide:
+    case Key::Kp_Multiply:
+    case Key::Kp_Subtract:
+    case Key::Kp_Add:
+    case Key::Kp_Enter:
+    case Key::Kp_Equal:
+    case Key::Left_Shift:
+    case Key::Left_Control:
+    case Key::Left_Alt:
+    case Key::Left_Super:
+    case Key::Right_Shift:
+    case Key::Right_Control:
+    case Key::Right_Alt:
+    case Key::Right_Super:
+    case Key::Menu:
+        return true;
+    }
+    return false;
+}
+
+void Window::key_callback(int key, int scancode, int action, int mods) {
+    auto vkey = static_cast<Key>(key);
+    VASSERT(key_valid(vkey), "An invalid key code was pressed.");
+    log_trace(
+        "Input Event: Key {}({}, {}) {} (Mods: {})",
+        magic_enum::enum_name<Key>(vkey),
+        key,
+        scancode,
+        action == GLFW_PRESS ? "Down"
+        : action == GLFW_RELEASE
+            ? "Up"
+            : "Repeat",
+        mods
+    );
+    if (action == GLFW_RELEASE) {
+        down_keys.erase(vkey);
+    } else {
+        down_keys.insert(vkey);
+    }
 }
 } // namespace vee::platform
--- Source/VeeRuntime/Public/Platform/Window.hpp
+++ Source/VeeRuntime/Public/Platform/Window.hpp
@@ -4,6 +4,8 @@
 
 #pragma once
 
+#include "Keys.hpp"
+
 #ifdef WIN32
 #include "WindowHandle.hpp"
 #endif
@@ -11,6 +13,7 @@
 
 #include "GLFW/glfw3.h"
 
+#include <set>
 #include <tuple>
 
 namespace vee::platform {
@@ -28,5 +31,13 @@ public:
     [[nodiscard]] WindowHandle get_handle() const;
 
     GLFWwindow* glfw_window = nullptr;
+
+    [[nodiscard]] bool is_key_down(Key key) const;
+
+private:
+    static void key_callback_dispatcher(GLFWwindow* glfw_window, int key, int scancode, int action, int mods);
+    void key_callback(int key, int scancode, int action, int mods);
+
+    std::set<Key> down_keys;
 };
 } // namespace vee::platform
